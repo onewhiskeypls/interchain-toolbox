e89be9f3a0594583b8ec6e2b243a75ae3d996bb3
whiskey <103599070+onewhiskeypls@users.noreply.github.com>
2023-04-12 20:16:51 -0700

    bug: correct msg sign encoding

--- src/util/common.ts
+++ src/util/common.ts
@@ -4,5 +4,5 @@ export function buildMsgSignerLink(
     signature: string
 ): string {
     const url = process.env.REACT_APP_PUBLIC_URL;
-    return encodeURI(`${url}/msgsigner?message=${message}&address=${address}&signature=${signature}`);
+    return encodeURI(`${url}/msgsigner?message=${encodeURIComponent(message)}&address=${encodeURIComponent(address)}&signature=${encodeURIComponent(signature)}`);
 }
